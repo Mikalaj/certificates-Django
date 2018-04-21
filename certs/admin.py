de00bd781aeb6663ab05442220bdf295d1d8f6c9
Mikalaj <fanclaus@gmail.com>
2018-04-21 17:11:07 +0300

    Update admin.py

--- certs/admin.py
+++ certs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Dignity, Clergy, Baptism, Wedding
 
 # Register your models here.
+admin.site.register(Dignity)
+admin.site.register(Clergy)
+admin.site.register(Baptism)
+admin.site.register(Wedding)
