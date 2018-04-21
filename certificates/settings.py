42f466e4f4ede5eb4d314014225f9c31e00c4104
Mikalaj <fanclaus@gmail.com>
2018-04-21 16:49:01 +0300

    Update settings.py

--- certificates/settings.py
+++ certificates/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'certs.apps.CertsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'certs'
 ]
 
 MIDDLEWARE = [
