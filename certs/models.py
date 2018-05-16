a76bf35ea6be484a265e45394d5db7d2367f9c6e
Mikalaj <fanclaus@gmail.com>
2018-05-16 17:20:48 +0300

    installed rest-framework, made initials REST setting

--- certs/models.py
+++ certs/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
-# Need to make a Validator for Dignity of Priests: "Іерэй", "Протаіерэй", etc 
+# Need to make a Validator for Dignity of Priests: "Іерэй", "Протаіерэй", etc
 class Dignity(models.Model):
     name = models.CharField(verbose_name="Сан", max_length=100, default="test")
     def __str__(self):
@@ -10,7 +11,7 @@ class Dignity(models.Model):
     class Meta:
         verbose_name = "Сан святара"
         verbose_name_plural = "Святарскія саны"
-#    
+#
 class Clergy(models.Model):
     dignity = models.OneToOneField(Dignity, on_delete=models.CASCADE)
     name = models.CharField(verbose_name="Імя святара", max_length=100, default="")
@@ -20,7 +21,7 @@ class Clergy(models.Model):
         verbose_name = "Святар"
         verbose_name_plural = "Святары"
 
-# Parent general class of certificates for Certificates of Baptism and Wedding    
+# Parent general class of certificates for Certificates of Baptism and Wedding
 class Certificate(models.Model):
     date = models.DateField(verbose_name="Дата")
     number = models.IntegerField(verbose_name="Нумар")
@@ -35,8 +36,10 @@ class Baptism(Certificate):
     saint_name = models.CharField(verbose_name="Імя святога", max_length=300)
     saint_date = models.DateField(verbose_name="Дзень Анёла")
     def __str__(self):
-        return "{} {} {}".format(self.baptized_name, self.baptized_middle_name, self.baptized_surname)
-    
+        return "{} {} {}".format(self.baptized_name,
+                                 self.baptized_middle_name,
+                                 self.baptized_surname)
+
 class Wedding(Certificate):
     fiance_name = models.CharField(verbose_name="Імя жаніха", max_length=30)
     fiance_middle_name = models.CharField(verbose_name="Імя па бацьку жаніха", max_length=30)
@@ -47,13 +50,9 @@ class Wedding(Certificate):
     witness1 = models.CharField(verbose_name="Сведка №1", max_length=100)
     witness2 = models.CharField(verbose_name="Сведка №2", max_length=100)
     def __str__(self):
-        return "{} {} {} і {} {} {}".format(self.fiance_name, 
-                                            self.fiance_middle_name, 
+        return "{} {} {} і {} {} {}".format(self.fiance_name,
+                                            self.fiance_middle_name,
                                             self.fiance_surname,
-                                            self.fiancee_name, 
-                                            self.fiancee_middle_name, 
+                                            self.fiancee_name,
+                                            self.fiancee_middle_name,
                                             self.fiancee_surname)
-    
-
-
-    
